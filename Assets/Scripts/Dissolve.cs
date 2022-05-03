1b7d51e529d933eefb8dd12c8878c3cec2d1e2cb
Tomasz Kalicki <tomics32@gmail.com>
2022-05-03 18:36:35 +0200

    Added "Level Completed" screen

--- Assets/Scripts/Dissolve.cs
+++ Assets/Scripts/Dissolve.cs
@@ -1,20 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Globalization;
 
 public class Dissolve : MonoBehaviour
 {
 
     Animator anim;
-  
+    public GameObject levelCompletedMenu;
+    [SerializeField] TextMeshProUGUI timeLeft;
+    [SerializeField] Text countDownText;
+    float timeToFloat;
+    [SerializeField] float[] starTime;
+    [SerializeField] Image[] stars;
+    Color myColor = new Color32(255, 255, 255, 100);
+
+
+
+
 
     private void Start()
     {
+        
         anim = gameObject.GetComponent<Animator>();
+        myColor = Color.white;
     }
+
+    
     private void OnTriggerEnter(Collider other)
     {
         anim.Play("Dissolve");
         gameObject.GetComponent<AudioSource>().Play();
+        Invoke("DelayedMenu", 1);
+        timeLeft.text = countDownText.text;
+        countDownText.gameObject.SetActive(false);
+        timeToFloat = float.Parse(timeLeft.text, CultureInfo.InvariantCulture);
+
+        
+
+        if(timeToFloat >= starTime[1])
+        {
+            stars[2].GetComponent<Image>().color = myColor;
+            stars[1].GetComponent<Image>().color = myColor;
+            stars[0].GetComponent<Image>().color = myColor;
+
+        }
+        else if (timeToFloat >= starTime[0] && timeToFloat < starTime[1])
+        {
+            stars[1].GetComponent<Image>().color = myColor;
+            stars[0].GetComponent<Image>().color = myColor;
+        }
+        else
+        {
+            stars[0].GetComponent<Image>().color = myColor;
+        }
+
+
+
+    }
+    public void DelayedMenu()
+    {     
+        levelCompletedMenu.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 }
